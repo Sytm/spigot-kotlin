e42d2449a4791a85473888e40369247cb131a39e
Lukas Planz <lukas.planz@web.de>
2020-11-21 11:40:06 +0100

    Bump Kotlin version to 1.4.20
    - Add ktorm
    - Fix variable expansion in process resources task
    - Fix dependabot.yml to use gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.*
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.20"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "de.md5lukas"
-version = "1.4.10-2"
+version = "1.4.20"
 description = "Kotlin for spigot"
 
 repositories {
@@ -36,11 +36,14 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.28.1")
     implementation("org.jetbrains.exposed:exposed-java-time:0.28.1")
     implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("org.ktorm:ktorm-core:3.2.0")
     //implementation("com.okkero.skedule:skedule:1.2.6")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.4.10")
 }
 
 tasks.withType<ProcessResources> {
+    // Force refresh, because gradle does not detect changes in the variables used by expand
+    this.outputs.upToDateWhen { false }
     expand("version" to project.version)
 }
 
@@ -55,6 +58,7 @@ tasks.withType<ShadowJar> {
         include(dependency("org.jetbrains.kotlin:.*"))
         include(dependency("org.jetbrains.kotlinx:.*"))
         include(dependency("org.jetbrains.exposed:.*"))
+        include(dependency("org.ktorm:.*"))
         include(dependency("com.zaxxer:HikariCP"))
         //include(dependency("com.okkero.skedule:skedule"))
     }
